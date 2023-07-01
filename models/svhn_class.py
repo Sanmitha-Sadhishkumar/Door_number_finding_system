bd9fb8edd083821fab4f4c13f666f3036ca6f370
unknown <sanmithasadhishkumar@gmail.com>
2023-07-01 17:11:19 +0530

    Model split, Model is working with 20% accuracy

---
+++ models/svhn_class.py
@@ -0,0 +1,67 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torchvision.datasets import SVHN
+from torchvision.transforms import ToTensor
+from torchvision.utils import make_grid
+from torch.utils.data.dataloader import DataLoader
+import svhn_func as SF
+
+class SvhnModel(nn.Module):
+    def __init__(self, in_size, out_size):
+        super().__init__()
+        self.linear1 = nn.Linear(in_size, 1024)
+        self.linear2 = nn.Linear(1024, 512)
+        self.linear3 = nn.Linear(512, 256)
+        self.linear4 = nn.Linear(256, 128)
+        self.linear5 = nn.Linear(128, 64)
+        self.linear6 = nn.Linear(64, 32)
+        self.linear7 = nn.Linear(32, out_size)
+        
+    def forward(self, xb):
+        xb = xb.view(xb.size(0),-1)
+        if xb.shape[1]==3072:
+            out = self.linear1(xb)
+            out = F.relu(out)
+            xb=out
+        out = self.linear2(xb)
+        out = F.relu(out)
+        out = self.linear3(out)
+        out = F.relu(out)
+        out = self.linear4(out)
+        out = F.relu(out)
+        out = self.linear5(out)
+        out = F.relu(out)
+        out = self.linear6(out)
+        out = F.relu(out)
+        out = self.linear7(out)
+        return out
+        
+    
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = SF.accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss, 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
+
+
+
--- models/svhn_dl_model.py
+++ models/svhn_dl_model.py
@@ -1,200 +1,29 @@
 import torch
-import torchvision
-import numpy as np
-import matplotlib.pyplot as plt
-import torch.nn as nn
-import torch.nn.functional as F
 from torchvision.datasets import SVHN
 from torchvision.transforms import ToTensor
-from torchvision.utils import make_grid
 from torch.utils.data.dataloader import DataLoader
-import pandas as pd
-from torch.utils.data import random_split
+import svhn_class as S
+import asyncio
+import svhn_func as SF
+import loss_accuracy as LA
 
-dataset = SVHN(root="E:/git/Door_number_finding_system/data", download=True, transform=ToTensor())
-
-val_size = 12000
-train_size = len(dataset) - val_size
-
-train_loader, val_loader = random_split(dataset, [train_size, val_size])
+test_ds=SVHN(root="E:/git/Door_number_finding_system/data",split='test', download=True, transform=ToTensor())
+train_ds=SVHN(root="E:/git/Door_number_finding_system/data", download=True, transform=ToTensor())
+history=[]
 
 batch_size=64
-#train_loader=pd.read_csv("E:/git/Door_number_finding_system/data/train/train")
-
-for images, _ in train_loader:
-    print('images.shape:', images.shape)
-    plt.figure(figsize=(16,8))
-    plt.axis('off')
-    plt.imshow(make_grid(images, nrow=16).permute((1, 2, 0)))
-    break
-    
-def accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-    
-class SvhnModel(nn.Module):
-    """Feedfoward neural network with 6 hidden layer"""
-    def __init__(self, in_size, out_size):
-        super().__init__()
-        # hidden layer
-        self.linear1 = nn.Linear(in_size, 1024)
-        self.linear2 = nn.Linear(1024, 512)
-        self.linear3 = nn.Linear(512, 256)
-        self.linear4 = nn.Linear(256, 128)
-        self.linear5 = nn.Linear(128, 64)
-        self.linear6 = nn.Linear(64, 32)
-        # output layer
-        self.linear7 = nn.Linear(32, out_size)
-        
-    def forward(self, xb):
-        # Flatten the image tensors
-        xb = xb.view(xb.size(0), -1)
-        # Get intermediate outputs using hidden layer
-        out = self.linear1(xb)
-        out = F.relu(out)
-        out = self.linear2(out)
-        out = F.relu(out)
-        out = self.linear3(out)
-        out = F.relu(out)
-        out = self.linear4(out)
-        out = F.relu(out)
-        out = self.linear5(out)
-        out = F.relu(out)
-        out = self.linear6(out)
-        out = F.relu(out)
-        # Get predictions using output layer
-        out = self.linear7(out)
-        return out
-    
-    def training_step(self, batch):
-        images, labels = batch 
-        out = self(images)                  # Generate predictions
-        loss = F.cross_entropy(out, labels) # Calculate loss
-        return loss
-    
-    def validation_step(self, batch):
-        images, labels = batch 
-        out = self(images)                    # Generate predictions
-        loss = F.cross_entropy(out, labels)   # Calculate loss
-        acc = accuracy(out, labels)           # Calculate accuracy
-        return {'val_loss': loss, 'val_acc': acc}
-        
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x['val_loss'] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
-        batch_accs = [x['val_acc'] for x in outputs]
-        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
-        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
-    
-    def epoch_end(self, epoch, result):
-        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
+train_loader=DataLoader(train_ds,batch_size,shuffle=True,num_workers=4,pin_memory=True)
+test_loader=DataLoader(test_ds,batch_size,num_workers=4,pin_memory=True)
 
 input_size = 3072
-num_classes = 10
+num_classes = 10   
 
-model = SvhnModel(input_size, out_size=num_classes)
+model = S.SvhnModel(input_size, out_size=num_classes)
 
-for t in model.parameters():
-    print(t.shape)
-    
-for images, labels in train_loader:
-    outputs = model(images)
-    loss = F.cross_entropy(outputs, labels)
-    print('Loss:', loss.item())
-    break
-
-print('outputs.shape : ', outputs.shape)
-print('Sample outputs :\n', outputs[:2].data)
-
-torch.cuda.is_available()
-
-def get_default_device():
-    """Pick GPU if available, else CPU"""
-    if torch.cuda.is_available():
-        return torch.device('cuda')
-    else:
-        return torch.device('cpu')
-        
-device = get_default_device()
-device
-
-def to_device(data, device):
-    """Move tensor(s) to chosen device"""
-    if isinstance(data, (list,tuple)):
-        return [to_device(x, device) for x in data]
-    return data.to(device, non_blocking=True)
-
-for images, labels in train_loader:
-    print(images.shape)
-    images = to_device(images, device)
-    print(images.device)
-    break
-    
-class DeviceDataLoader():
-    """Wrap a dataloader to move data to a device"""
-    def __init__(self, dl, device):
-        self.dl = dl
-        self.device = device
-        
-    def __iter__(self):
-        """Yield a batch of data after moving it to device"""
-        for b in self.dl: 
-            yield to_device(b, self.device)
-
-    def __len__(self):
-        """Number of batches"""
-        return len(self.dl)
-        
-train_loader = DeviceDataLoader(train_loader, device)
-val_loader = DeviceDataLoader(val_loader, device)
-
-for xb, yb in val_loader:
-    print('xb.device:', xb.device)
-    print('yb:', yb)
-    break
-    
-def evaluate(model, val_loader):
-    outputs = [model.validation_step(batch) for batch in val_loader]
-    return model.validation_epoch_end(outputs)
-
-def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
-    history = []
-    optimizer = opt_func(model.parameters(), lr)
-    for epoch in range(epochs):
-        # Training Phase 
-        for batch in train_loader:
-            loss = model.training_step(batch)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-        # Validation phase
-        result = evaluate(model, val_loader)
-        model.epoch_end(epoch, result)
-        history.append(result)
-    return history
-    
-# Model (on GPU)
-model = SvhnModel(input_size, out_size=num_classes)
-to_device(model, device)
-
-history = [evaluate(model, val_loader)]
-history
-
-history += fit(10,1e-2, model, train_loader, val_loader)
-
-history += fit(10,1e-3, model, train_loader, val_loader)
-
-import matplotlib.pyplot as plt
-#matplotlib inline
-
-losses = [x['val_loss'] for x in history]
-plt.plot(losses, '-x')
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.title('Loss vs. No. of epochs');
-
-accuracies = [x['val_acc'] for x in history]
-plt.plot(accuracies, '-x')
-plt.xlabel('epoch')
-plt.ylabel('accuracy')
-plt.title('Accuracy vs. No. of epochs');
+if __name__=='__main__':
+    torch.multiprocessing.freeze_support()
+    SF.loadModel(train_loader,model)
+    torch.multiprocessing.freeze_support()
+    history = asyncio.run(SF.model_val(train_loader,test_loader,model))
+    LA.loss(history)
+    LA._accuracy(history)
