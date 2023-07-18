74b6b18ba8b0971c587d38d330db161eaba492a9
unknown <sanmithasadhishkumar@gmail.com>
2023-07-18 13:25:35 +0530

    dataset

--- api/ocr.py
+++ api/ocr.py
@@ -1,6 +1,13 @@
 from paddleocr import PaddleOCR,draw_ocr
 ocr = PaddleOCR(lang='en') # need to run only once to download and load model into memory
-img_path = r"C:\Users\Sanmitha\Documents\third.jpg"
+from PIL import Image
+
+img1= Image.open(r"E:\git\Door_number_finding_system\api\images\second.jpg")
+h,w=img1.size
+img1.save(r"E:\git\Door_number_finding_system\api\original.png")
+
+img_path = r"E:\git\Door_number_finding_system\api\original.png"
+
 result = ocr.ocr(img_path, cls=False)
 for idx in range(len(result)):
     res = result[idx]
@@ -8,7 +15,7 @@ for idx in range(len(result)):
         print(line)
 
 # draw result
-from PIL import Image
+
 result = result[0]
 image = Image.open(img_path).convert('RGB')
 boxes = [line[0] for line in result]
